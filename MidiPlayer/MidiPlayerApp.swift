4cad0aa7ef50c0bda61926921d21fe5ad93090d0
Lindar Olostur <lindarolostur@gmail.com>
2025-11-27 16:30:09 +0300

    Initial Commit

---
+++ MidiPlayer/MidiPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MidiPlayerApp.swift
+//  MidiPlayer
+//
+//  Created by Lindar Olostur on 27.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct MidiPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
